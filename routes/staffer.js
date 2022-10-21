1e7c6e752ffbd903262598cc5a588ee96580237d
Rafael Gabriel Arceo <arceo.rafaelgabriel@protonmail.com>
2022-10-21 17:06:52 +0000

    Added working information flow from the router toward the controller.

--- routes/staffer.js
+++ routes/staffer.js
@@ -1,3 +1,11 @@
-import {express, router} from "express"
-import { StafferController } from "../controllers/staffer_controller"
+import express, { application } from "express"
+import {staffer_list} from "../controllers/staffer_controller.js";
 
+const router = express.Router();
+
+// Handles the initial get request staffer_list
+router.get("/", staffer_list);
+
+router.get("/:staffer_id")
+
+export {router};
